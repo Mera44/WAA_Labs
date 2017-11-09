9c15bb8ec085c0517b4b667479d31ac5cc66960a
Mera44 <Mera@meras-MacBook-Pro.local>
2017-11-08 18:52:34 -0600

    lab 6 finished

---
+++ Lab5_Valid/src/main/java/com/students/domain/Phone.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.students.domain;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+/**
+ * @author B.Pirasanth
+ *
+ */
+public class Phone implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@NotNull
+	@Range(min = 100, max = 999, message = "{phone.areacode}")
+	private Integer area;
+	@NotNull
+	// @Range(min=100, max=999, message="{phone.prefixcode}")
+
+	private Integer prefix;
+	@NotNull
+	@Range(min = 1001, max = 9999, message = "{phone.numbercode}")
+	private Integer number;
+
+	public Integer getArea() {
+		return area;
+	}
+
+	public void setArea(Integer area) {
+		this.area = area;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Integer getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(Integer prefix) {
+		this.prefix = prefix;
+	}
+}
